0a773d7e0fd11a1ee79577124b059286b07cc2f3
komp <hthrvojetrdin@gmail.com>
2024-05-27 22:14:45 +0200

    kreiranje racunera

---
+++ racunera.sql
@@ -0,0 +1,19 @@
+--create database racunera; 
+
+--use racunera;
+
+--create table racun(
+--sifra int,
+--rednibroj int,
+--datum datetime,
+--kupac varchar(45),
+--placeno bit
+--);
+
+--create table stavkeracuna(
+--racun int,
+--artikl varchar(50),
+--cijena decimal(18,2),
+--kolicina int,
+--popust decimal(18,2)
+--);
