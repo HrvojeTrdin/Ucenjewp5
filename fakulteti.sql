9c6b5a20466a018908acfdff168375784efb2f81
komp <hthrvojetrdin@gmail.com>
2024-05-27 21:56:36 +0200

    kreiranje fakulteti

---
+++ fakulteti.sql
@@ -0,0 +1,15 @@
+--create database fakulteti;
+
+--create table ispitnirok(
+--sifra int,
+--predmet varchar(25),
+--vrstaispita varchar(40),
+--datum datetime,
+--pristupio varchar(50)
+--);
+--create table pristupnici(
+--ispitnirok datetime,
+--student varchar(50),
+--brojbodova int,
+--ocjena int
+--);
